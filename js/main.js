69131d5eb7710850d9f980bb5386e64350145435
Maksym D <max.moderation@gmail.com>
2020-09-21 13:08:04 +0300

    office commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.my-slick__inner').slick({
+        arrows: true,
+        // dots: false,
+        infinite: true,
+        // adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:900,
+        easing:'ease', // animation
+        // initialSlide:1,
+        // autoplay:true,
+        // // autoplaySpeed:1200,
+        // pauseOnFocus:true,
+        // pauseOnHover:true,
+        // pauseOnDotsHover:true,
+        // draggable:false,
+        // swipe:false, // swipe for mobiles
+
+
+    });
+});
