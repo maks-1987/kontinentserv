5b6a5fb69d7c5c68a90b73553a8d0eb211518b3f
MaxD <max.moderation@gmail.com>
2020-09-25 20:10:56 +0300

    fix slick

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 $(function () {
     $('.my-slick__inner').slick({
         arrows: true,
-<<<<<<< HEAD
-        // dots: false,
-        infinite: true,
-        // adaptiveHeight:true,
-        slidesToShow:1,
-        slidesToScroll:1,
-        speed:900,
-        easing:'ease', // animation
-        // initialSlide:1,
-        // autoplay:true,
-        // // autoplaySpeed:1200,
-        // pauseOnFocus:true,
-        // pauseOnHover:true,
-        // pauseOnDotsHover:true,
-=======
         // dots: true,
         infinite: true,
         // adaptiveHeight:true,
@@ -29,7 +14,6 @@ $(function () {
         pauseOnFocus:true,
         pauseOnHover:true,
         pauseOnDotsHover:true,
->>>>>>> ebfbfcdf4050b574f56d954febc163e08d084c48
         // draggable:false,
         // swipe:false, // swipe for mobiles
 
