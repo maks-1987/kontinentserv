b445a79ee794735eb1ea776f890e4dfe38b2a078
MaxD <max.moderation@gmail.com>
2020-10-07 22:28:42 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(function () {
     $('.my-slick__inner').slick({
-        arrows: true,
+        // arrows: true,
         // dots: true,
         // fade: true,
-        infinite: true,
-        variableWidth: true,
-        adaptiveHeight:true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        speed: 900,
-        easing: 'ease', // animation
-        initialSlide:1,
-        autoplay: true,
-        autoplaySpeed: 3500,
-        pauseOnFocus:true,
-        pauseOnHover:true,
-        pauseOnDotsHover:true,
+        // infinite: true,
+        // variableWidth: true,
+        // adaptiveHeight:true,
+        // slidesToShow: 2,
+        // slidesToScroll: 2,
+        // speed: 900,
+        // easing: 'ease', // animation
+        // initialSlide:1,
+        // autoplay: true,
+        // autoplaySpeed: 3500,
+        // pauseOnFocus:true,
+        // pauseOnHover:true,
+        // pauseOnDotsHover:true,
         // draggable:false,
         // swipe:false, // swipe for mobiles
 
